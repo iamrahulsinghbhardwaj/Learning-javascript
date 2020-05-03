cfd2fefa58bef11478fefe5e9b63c6c5c6be2195
Rahul Singh <42406373+iamrahulsinghbhardwaj@users.noreply.github.com>
2020-05-03 13:37:40 +0530

    Update checkbox.js

--- JS Bootcamp/JS In Browser/checkbox/checkbox.js
+++ JS Bootcamp/JS In Browser/checkbox/checkbox.js
@@ -1,1 +1,74 @@
 
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+const filters = {
+    searchText: '',
+    hidecompleted: false
+}
+
+const renderTodos = function (todos, filters) {
+    let filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    });
+
+    filteredTodos=filteredTodos.filter(function(todo){
+        if(filters.hidecompleted){
+            return !todo.completed;
+        }else{
+            return true;
+        }
+    });
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+});
+
+document.querySelector('#hide-completed').addEventListener('change',function(e){
+    filters.hidecompleted=e.target.checked;
+    renderTodos(todos,filters);
+})
