877e8554b363c943b9ea086ed5e102fba9fb1966
Rahul Singh <42406373+iamrahulsinghbhardwaj@users.noreply.github.com>
2020-05-04 19:23:58 +0530

    Update notes-function.js

--- JS Bootcamp/Datastorages,Libraries/Splitting application code/notes-function.js
+++ JS Bootcamp/Datastorages,Libraries/Splitting application code/notes-function.js
@@ -1,1 +1,21 @@
+const getSavedNotes=function(){
+	const notesJSON=localStorage.getItem('notes');
+	if(notesJSON!=null){
+		return JSON.parse(notesJSON);
+	}else{
+		return [];	
+	}
+}
 
+//DOM methord
+
+const generateNoteDom=function(note){
+	const noteEl=document.createElement('p');
+
+	if(note.title.length>0){
+		noteEl.textContent=note.title;
+	}else{
+		noteEl.textContent='Unnamed Note';
+	}
+	return noteEl;
+}
