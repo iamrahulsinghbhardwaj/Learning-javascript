dfe646c622639aed940f95928481d72590a71ff7
Rahul Singh <42406373+iamrahulsinghbhardwaj@users.noreply.github.com>
2020-05-16 17:06:43 +0530

    Add files via upload

---
+++ JS Bootcamp/E JS k/type-coresion.js
@@ -0,0 +1,11 @@
+console.log(5+'5');//55
+console.log('5'-5);//0
+console.log(5=='5');//true
+console.log(5==='5')//false
+
+let type=typeof {}
+console.log(type);
+
+const value=true+12; //true=1 flase=1
+type=typeof value
+console.log(`${type} ${value}`);
