2bbb336f51c46159b7c0135a5967d7656727ca92
Rahul Singh <42406373+iamrahulsinghbhardwaj@users.noreply.github.com>
2020-06-09 14:35:56 +0530

    Update Getters and setters.js

--- JS Bootcamp/OOP/Getters and setters.js
+++ JS Bootcamp/OOP/Getters and setters.js
@@ -1,1 +1,14 @@
+const data={
+    locations: [],
+    get location(){
+        return this._location
+    },
+    set location(value){
+        this._location=value.trim()
+        this.locations.push(this._location)
+    }
+}
 
+data.location='Pathankot'
+data.location='Jugial'
+console.log(data)
